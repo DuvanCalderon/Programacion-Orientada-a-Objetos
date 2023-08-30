10860fe84de081dbcc6b85b825d342f5820cbaad
DuvanCalderon <79523677+DuvanCalderon@users.noreply.github.com>
2023-08-30 09:11:31 -0500

    Add files via upload

---
+++ JavaApplication11.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication11;
+
+/**
+ *
+ * @author duvan
+ */
+public class JavaApplication11 {
+
+
+    public static void main(String[] args) {
+        byte a = 8;
+        byte b = 5;
+        System.out.println(a + b); 
+        
+                
+
+    }
+    
+}

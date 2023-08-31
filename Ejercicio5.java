2ecc800cafeedd9a850cbf18f8e0ffac127215a7
DuvanCalderon <79523677+DuvanCalderon@users.noreply.github.com>
2023-08-30 22:13:41 -0500

    Create Ejercicio5.java

---
+++ Ejercicio5.java
@@ -0,0 +1,19 @@
+package EJercicio5;
+
+
+public class Ejercicio5 {
+
+
+    public static void main(String[] args) {
+        double suma, x, y;
+        suma = 0;
+        x = 20;
+        y= 40;
+        suma = x + suma;
+        x = x + Math.pow(y, 2);
+        suma = suma + (x/y);
+        System.out.println("El valor de la suma es: " + suma);
+                                            
+    }
+    
+}
